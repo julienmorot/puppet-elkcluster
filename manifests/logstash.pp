ddd8a1a580e50b0ff10313db399fdcdb0b5d4e97
Julien Morot <jmorot@gmail.com>
2018-12-19 13:03:37 +0100

    Workaround with java 10 and logstash JVM option

---
+++ manifests/logstash.pp
@@ -0,0 +1,48 @@
+class elkcluster::logstash (
+) {
+
+    include ::elkcluster
+    ensure_packages(['default-jre'], { ensure => present })
+
+    File { '/etc/logstash':
+        ensure => directory,
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0775',
+    }
+
+#   File { '/etc/logstash/logstash.yml':
+#       ensure  => file,
+#       owner   => 'root',
+#       group   => 'root',
+#       mode    => '0644',
+#       content => template("${module_name}/logstash.yml.erb"),
+#       require => Package['logstash'],
+#       notify  => Service['logstash'],
+#   }
+
+   File { '/etc/logstash/jvm.options':
+       ensure  => file,
+       owner   => 'root',
+       group   => 'root',
+       mode    => '0644',
+       content => template("${module_name}/logstash/jvm.options.erb"),
+       require => File['/etc/logstash'],
+       notify  => Service['logstash'],
+   }
+
+    # Workaround because logstash doesn't start with UseParNewGC JVM options
+    #And the behaviour prevent apt to finish logtash installation
+    Package { 'logstash':
+        ensure   => present,
+        require => File['/etc/logstash/jvm.options'],
+    }
+
+    Service { 'logstash':
+        ensure  => 'running',
+        enable  => 'true',
+        require => File['/etc/logstash/jvm.options']
+    }
+
+}
+
